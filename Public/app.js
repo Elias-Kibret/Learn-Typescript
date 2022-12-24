0461d7abb889dd49c7b146deef0482a647e78dcd
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-12-24 11:55:29 +0300

    classses

--- Public/app.js
+++ Public/app.js
@@ -5,6 +5,20 @@
 // }
 // console.log(anchor.href)
 // const form=document.querySelector('form')
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes E${this.amount}`;
+    }
+}
+const invoiceOne = new Invoice('elias', 'work', 200);
+const invoiceTwo = new Invoice('Kibret', 'work', 300);
+let invoice = [];
+console.log(invoiceOne, invoiceTwo);
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector("#type");
 const toForm = document.querySelector("#tofrom");
---
+++ src/app.ts
@@ -0,0 +1,52 @@
+// const anchor = document.querySelector('a')!
+// if (anchor) {
+    
+//     console.log(anchor.href)
+// }
+// console.log(anchor.href)
+
+// const form=document.querySelector('form')
+
+
+class Invoice{
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount=a
+    }
+    format() {
+        return `${this.client} owes E${this.amount}`
+    }
+}
+
+const invoiceOne = new Invoice('elias', 'work', 200)
+const invoiceTwo = new Invoice('Kibret', 'work', 300)
+
+let invoice: Invoice[] = []
+
+console.log(invoiceOne,invoiceTwo)
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement
+const toForm = document.querySelector("#tofrom") as HTMLSelectElement
+const details = document.querySelector("#details") as HTMLSelectElement
+const amount=document.querySelector("#amount") as HTMLInputElement
+
+
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+    console.log(
+        type.value,
+        toForm.value,
+        details.value,
+        amount.valueAsNumber
+    )
+}
+    )
+    
