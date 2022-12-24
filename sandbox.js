7113450276f0e3d97b589848932d6bfeafcf5cb5
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-12-24 09:22:36 +0300

    Object and array type

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,34 @@
-var character = "Elias";
-var age = 30;
-var isBlackBelt = false;
-// character=20
-character = "kibret";
-age = 40;
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// let character = "Elias"
+// let age = 30
+// let isBlackBelt = false
+// // character=20
+// character = "kibret"
+// age = 40
+// isBlackBelt = true
+// const circ = (diameter:number) => {
+// return diameter*Math.PI
+// }
+// console.log(circ(30))
+var names = ['luig', 'mario', 'yoshi'];
+names.push('toald');
+// names.push(3)
+// names[0]=40
+var numbers = [10, 20, 30, 40];
+// numbers.push(24)
+// numbers.push('elias')
+var mixed = ['ken', '4', 'chun', 8, 9];
+mixed[0] = 3;
+// Objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(30));
+ninja.age = 40;
+ninja.name = 'elias';
+// ninja.skills = ['fignting', 'sneaking']
+ninja = {
+    name: 'elias',
+    belt: 'orange',
+    age: 30
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,15 +1,42 @@
-let character = "Elias"
-let age = 30
-let isBlackBelt = false
-// character=20
-character = "kibret"
-age = 40
+// let character = "Elias"
+// let age = 30
+// let isBlackBelt = false
+// // character=20
+// character = "kibret"
+// age = 40
 
-isBlackBelt = true
+// isBlackBelt = true
 
 
-const circ = (diameter:number) => {
-return diameter*Math.PI
+// const circ = (diameter:number) => {
+// return diameter*Math.PI
+// }
+
+// console.log(circ(30))
+
+
+let names = ['luig', 'mario', 'yoshi']
+names.push('toald')
+// names.push(3)
+// names[0]=40
+let numbers = [10, 20, 30, 40]
+// numbers.push(24)
+// numbers.push('elias')
+
+let mixed = ['ken', '4', 'chun', 8, 9]
+mixed[0] = 3
+
+// Objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age:30
 }
-
-console.log(circ(30))
+ninja.age = 40
+ninja.name = 'elias'
+// ninja.skills = ['fignting', 'sneaking']
+ninja = {
+    name: 'elias',
+    belt: 'orange',
+    age:30
+}
